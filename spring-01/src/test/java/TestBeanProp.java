4ce370480b96de9fdf44193cbe0aa409d1410f82
jack <uplinkxu@gmail.com>
2018-07-19 22:36:26 -0400

    add config for prop

---
+++ spring-01/src/test/java/TestBeanProp.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.sql.DataSource;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author jack
+ */
+public class TestBeanProp {
+
+    private ApplicationContext ctx = null;
+
+    public TestBeanProp() {
+        ctx = new ClassPathXmlApplicationContext("appConfig-prop.xml");
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void testDS() {
+        DataSource ds=(DataSource) ctx.getBean("dataSource");
+        System.out.println("ds:"+ds.toString());
+    }
+}
