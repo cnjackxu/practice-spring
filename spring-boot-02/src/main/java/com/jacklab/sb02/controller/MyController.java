9b5f73a8320fd6e75910310e872ac36d31e6105b
jack <uplinkxu@gmail.com>
2018-08-27 21:31:48 -0400

    add spring boot 02

---
+++ spring-boot-02/src/main/java/com/jacklab/sb02/controller/MyController.java
@@ -0,0 +1,27 @@
+package com.jacklab.sb02.controller;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "hello~";
+	}
+	
+	// No @ResponseBody
+	@RequestMapping("/success")
+	public String success(Map<String,Object> param) {
+		param.put("name", "<b>Jack Xu</b>");
+		param.put("users", Arrays.asList("aaa","bbb","ccc"));
+		//classpath:templates/*.html
+		return "success";
+	}
+}
